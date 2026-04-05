4dbbe9a6397104191131bc5eb531af0eba53cd6f
Harshad Shewale <uselesslap123@gmail.com>
2026-04-05 13:16:50 +0000

    Try fixing this error: `Runtime SyntaxError: Unexpected end of JSON inpu

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -73,6 +73,18 @@ const SidebarProvider = React.forwardRef<
     // This is the internal state of the sidebar.
     // We use openProp and setOpenProp for control from outside the component.
     const [_open, _setOpen] = React.useState(defaultOpen)
+
+    React.useEffect(() => {
+      const cookieValue = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith(`${SIDEBAR_COOKIE_NAME}=`))
+        ?.split("=")[1]
+
+      if (cookieValue !== undefined) {
+        _setOpen(cookieValue === "true")
+      }
+    }, [])
+
     const open = openProp ?? _open
     const setOpen = React.useCallback(
       (value: boolean | ((value: boolean) => boolean)) => {
